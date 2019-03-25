aa5dc6c96316713daec8660a48acac5dd11b6149
marcos.dealmeida <blackheal@gmail.com>
2019-03-25 15:34:58 -0300

    Implement Interfaces for services. Closes #5

---
+++ petclinic-data/src/main/java/com/almeida/springframework/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.almeida.springframework.petclinic.services;
+
+import com.almeida.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
