b1b20213f488d384c65c5d5504404ddfd4ae145a
marcos.dealmeida <blackheal@gmail.com>
2019-03-25 15:38:22 -0300

    ...

---
+++ petclinic-data/src/main/java/com/almeida/springframework/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.almeida.springframework.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ petclinic-data/src/main/java/com/almeida/springframework/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.almeida.springframework.petclinic.model;
+
+public class Vet extends Person {
+}
