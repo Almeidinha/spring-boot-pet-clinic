78b1e4a0531038d0be12e79ea6bf7dccd7622c3c
marcos.dealmeida <blackheal@gmail.com>
2019-03-26 11:21:21 -0300

    Create Vet Index page and Controller. Closes #7

---
+++ petclinic-web/src/main/java/com/almeida/springframework/petclinic/controllers/vetController.java
@@ -0,0 +1,13 @@
+package com.almeida.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class vetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.hml"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
