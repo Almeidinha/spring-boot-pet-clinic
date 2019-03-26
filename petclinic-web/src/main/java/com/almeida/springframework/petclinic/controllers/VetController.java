8ae868e6ec042c5765aa657b4b67dd259143d460
marcos.dealmeida <blackheal@gmail.com>
2019-03-26 17:08:56 -0300

    List all Vets on ve index page. Closes #15

---
+++ petclinic-web/src/main/java/com/almeida/springframework/petclinic/controllers/VetController.java
@@ -0,0 +1,23 @@
+package com.almeida.springframework.petclinic.controllers;
+
+import com.almeida.springframework.petclinic.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.hml"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+        return "vets/index";
+    }
+}
