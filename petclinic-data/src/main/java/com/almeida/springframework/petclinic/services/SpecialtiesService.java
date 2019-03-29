7f4173103b89c9a258da83e3822d945058c5371c
marcos.dealmeida <blackheal@gmail.com>
2019-03-29 14:02:10 -0300

    Create Specialty Map Service Impl. Closes #31

---
+++ petclinic-data/src/main/java/com/almeida/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.almeida.springframework.petclinic.services;
+
+import com.almeida.springframework.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
