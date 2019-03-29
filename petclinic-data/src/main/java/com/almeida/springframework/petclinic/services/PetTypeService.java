d134a987668b9478de05fcaaaa4491e5d0ff8de9
marcos.dealmeida <blackheal@gmail.com>
2019-03-29 10:46:59 -0300

    Create PetType Map Service Impl. Closes #29

---
+++ petclinic-data/src/main/java/com/almeida/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.almeida.springframework.petclinic.services;
+
+import com.almeida.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
