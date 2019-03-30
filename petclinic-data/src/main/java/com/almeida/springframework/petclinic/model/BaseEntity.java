9cca402ea1a6cf0fff2f9aaf618d9a7cbfa89d0e
marcos.dealmeida <blackheal@gmail.com>
2019-03-30 12:51:53 -0300

    Implement base Entity (JPA). Closes #45

--- petclinic-data/src/main/java/com/almeida/springframework/petclinic/model/BaseEntity.java
+++ petclinic-data/src/main/java/com/almeida/springframework/petclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package com.almeida.springframework.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass // Used to tell JPA this will be inherited, but should not be mapped to the database
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
