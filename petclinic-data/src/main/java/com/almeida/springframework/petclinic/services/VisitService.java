7f7c33198e59c152f07a750cbd98425c9031c2ac
marcos.dealmeida <blackheal@gmail.com>
2019-03-30 18:45:11 -0300

    Implemented a Visit Map Service. Closes #46

---
+++ petclinic-data/src/main/java/com/almeida/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.almeida.springframework.petclinic.services;
+
+import com.almeida.springframework.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
